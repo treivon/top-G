230a8d264a0e3c9722c956053e6366ba634d37c0
Opilane TTHK <opilane@tthk.ee>
2022-11-16 11:11:04 +0200

    dfgkjassksh

--- 16.11.2022/ConsoleApp1/ConsoleApp1/Program.cs
+++ 16.11.2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,10 +4,37 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        private static object testc;
+
+        public static string number { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your favourite football player t-shirt number!");
+            string  number = Console.ReadLine();
+            int numberParsed = Int16.Parse(number);
+
+            //teen siia if ja else loogikaga vastused
+
+           if (numberParsed <7)
+            {
+                Console.WriteLine("kaka");
+            }
+           else if (numberParsed > 7 && numberParsed <50)
+            {
+                Console.WriteLine("kaka2");
+            }
+           else if (numberParsed > 51 && numberParsed < 100 )
+            {
+                Console.WriteLine("kaka3");
+            }
+
+
+
+
+
+
+
         }
     }
 }
